79f999fa06b5c09daaca425ee5ae0471c9d44f5d
John Häggkvist <john.haggkvist@gmail.com>
2012-06-27 10:55:09 +0200

    remove enter

--- konami.js
+++ konami.js
@@ -1,7 +1,7 @@
 function Konami(callback)
 { 
 	var sequence = [
-		38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13
+		38, 38, 40, 40, 37, 39, 37, 39, 66, 65
 	], current = 0;
 
 	this.trigger = function(event) {
